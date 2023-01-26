5eec067e89f6db3beade215a9b575651bedfd73b
nprimo <primo.niccolo@gmail.com>
2023-01-26 17:22:25 +0100

    fix(run_container.sh): add name to container

--- run_container.sh
+++ run_container.sh
@@ -3,10 +3,11 @@
 set -e 
 
 PORT=3490
+CONTAINER_NAME="ciaomamma"
 
 if [[ $(docker ps | grep tester) ]]; then
-    docker kill tester
+    docker kill $CONTAINER_NAME
 fi
 
 docker build -t webserv .
-docker run -dp $PORT:$PORT webserv
+docker run --name $CONTAINER_NAME -dp $PORT:$PORT webserv
