8180cf6312c38b1a7604d75bda2093ba121e46f3
nprimo <primo.niccolo@gmail.com>
2023-03-01 19:50:21 +0100

    feat(main): change structure of main
    Prepare to launch several servers with different configs

--- srcs/server/main.cpp
+++ srcs/server/main.cpp
@@ -7,10 +7,11 @@ int main(int argc, char **argv)
 		std::cerr << "Usage: ./webserv [configuration file]" << std::endl;
 		return (1);
 	}
-	Server server;
-	// Use argv[1] to load config file onto server
-	server.load_config(argv[1]);
 	(void)argv;
+	// Parse config file and store all server configs in an array of Config
+	// For each Config, start a server with its own specific config
+	Config config;
+	Server server = Server(config);
 	try {
 		server.run();
 	} catch (std::exception & e) {
