df189d768aa641ab1843768c2133f885f7e5c361
nprimo <primo.niccolo@gmail.com>
2023-03-15 20:36:41 +0000

    feat(Config): add _default_error_pages
    - add new attribute to Class Config
    - add new method getDefatultErrorPage

--- includes/Config.hpp
+++ includes/Config.hpp
@@ -1,10 +1,13 @@
 #ifndef CLASS_CONFIG_HPP
 # define CLASS_CONFIG_HPP
 
+# include "utils.hpp"
+
 class Config {
 private:
 	unsigned long	_client_max_body_size;
 	const char*		_listening_port;
+	std::map<StatusCode, std::string>	_default_error_pages; 
 
 public:
 	Config();
@@ -15,6 +18,7 @@ public:
 
 	unsigned long	getClientMaxBodySize(void);
 	const char*		getListeningPort(void);
+	std::string		getDefaultErrorPage(StatusCode status_code);
 };
 
 #endif
--- srcs/server/Config.cpp
+++ srcs/server/Config.cpp
@@ -3,6 +3,17 @@
 Config::Config() {
 	_client_max_body_size = 1024; // in the future is going to come from the config file
 	_listening_port = "3491";
+	_default_error_pages[BAD_REQUEST] = "";
+	_default_error_pages[UNAUTHORIZED] = "";
+    _default_error_pages[BAD_REQUEST] = "";
+    _default_error_pages[UNAUTHORIZED] = "";
+    _default_error_pages[FORBIDDEN] = "";
+    _default_error_pages[NOT_FOUND] = "";
+    _default_error_pages[LENGTH_REQUIRED] = "";
+    _default_error_pages[CONTENT_TOO_LARGE] = "";
+    _default_error_pages[UNSUPPORTED_MEDIA_TYPE] = "";
+    _default_error_pages[INTERNAL_SERVER_ERROR] = "";
+    _default_error_pages[HTTP_VERSION_NOT_SUPPORTED] = "";
 }
 
 Config::Config(const Config& other) {
@@ -15,6 +26,7 @@ Config::~Config() {
 
 Config&	Config::operator=(const Config& other) {
 	this->_client_max_body_size = other._client_max_body_size;
+	// how to copy a map? -> for _default_error_pages 
 	return *this;
 }
 
@@ -25,4 +37,9 @@ unsigned long	Config::getClientMaxBodySize(void) {
 
 const char *	Config::getListeningPort(void) {
 	return _listening_port;
+}
+
+std::string		Config::getDefaultErrorPage(StatusCode status_code)
+{
+	return _default_error_pages[status_code];
 }
