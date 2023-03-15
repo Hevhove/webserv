9cbd0cdfe18e0fb4ec2e15eea0d88560d798415f
nprimo <primo.niccolo@gmail.com>
2023-03-15 20:40:19 +0000

    feat(Config): add default values for error pages

--- srcs/server/Config.cpp
+++ srcs/server/Config.cpp
@@ -3,17 +3,17 @@
 Config::Config() {
 	_client_max_body_size = 1024; // in the future is going to come from the config file
 	_listening_port = "3491";
-	_default_error_pages[BAD_REQUEST] = "";
-	_default_error_pages[UNAUTHORIZED] = "";
-    _default_error_pages[BAD_REQUEST] = "";
-    _default_error_pages[UNAUTHORIZED] = "";
-    _default_error_pages[FORBIDDEN] = "";
-    _default_error_pages[NOT_FOUND] = "";
-    _default_error_pages[LENGTH_REQUIRED] = "";
-    _default_error_pages[CONTENT_TOO_LARGE] = "";
-    _default_error_pages[UNSUPPORTED_MEDIA_TYPE] = "";
-    _default_error_pages[INTERNAL_SERVER_ERROR] = "";
-    _default_error_pages[HTTP_VERSION_NOT_SUPPORTED] = "";
+	_default_error_pages[BAD_REQUEST] = "BAD_REQUEST";
+	_default_error_pages[UNAUTHORIZED] = "UNAUTHORIZED";
+    _default_error_pages[BAD_REQUEST] = "BAD_REQUEST";
+    _default_error_pages[UNAUTHORIZED] = "UNAUTHORIZED";
+    _default_error_pages[FORBIDDEN] = "FORBIDDEN";
+    _default_error_pages[NOT_FOUND] = "NOT_FOUND";
+    _default_error_pages[LENGTH_REQUIRED] = "LENGTH_REQUIRED";
+    _default_error_pages[CONTENT_TOO_LARGE] = "CONTENT_TOO_LARGE";
+    _default_error_pages[UNSUPPORTED_MEDIA_TYPE] = "UNSUPPORTED_MEDIA_TYPE";
+    _default_error_pages[INTERNAL_SERVER_ERROR] = "INTERNAL_SERVER_ERROR";
+    _default_error_pages[HTTP_VERSION_NOT_SUPPORTED] = "HTTP_VERSION_NOT_SUPPORTED";
 }
 
 Config::Config(const Config& other) {
